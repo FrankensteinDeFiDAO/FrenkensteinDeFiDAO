d8f8f06adb07cd73c588dac41e99b8401e009a82
Albert Garuda <algarud@gmail.com>
2022-04-08 23:59:19 +0300

    min cleanup

--- webapp/src/components/Account.js
+++ webapp/src/components/Account.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { ethers } from 'ethers'
 
 function Account() {
-    const [account, setCurrentAccount] = React.useState(null);
-    const [chain, setChain] = React.useState(null);
+    const [account, setCurrentAccount] = useState(null);
+    const [chain, setChain] = useState(null);
 
     const setWallet = async () => {
         try {
@@ -23,7 +23,7 @@ function Account() {
             console.log("Found an authorized account:", account);
             setCurrentAccount(account);
           } else {
-            console.log("No authorized account found")
+            console.log("No authorized account found");
           }
         } catch (error) {
           console.log(error);
@@ -31,21 +31,19 @@ function Account() {
       }
 
     async function setChainName() {
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        const network = await provider.getNetwork()
-        const chainString = network.name + "(" + network.chainId + ")"
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const network = await provider.getNetwork();
+        const chainString = network.name + "(" + network.chainId + ")";
         setChain(chainString);
     }
 
-    React.useEffect(() => {
+    useEffect(() => {
         setWallet();
         setChainName();
-      }, [])
+      }, []);
 
     if (account) return (<><div>Account: {account}</div><div>on {chain}</div> </>);
     else return(<div>onboarding btn</div>);
 }
-
-
 
 export default Account;
