f3fa6c5c53af1ea61e922ec0d728050c0137299f
Albert Garuda <algarud@gmail.com>
2022-04-08 23:03:01 +0300

    init navbar

---
+++ webapp/src/components/NavigationBar.js
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BUSL-1.1
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Container, Navbar } from 'react-bootstrap'
+// import Account from './Account'
+
+function NavigationBar(props) {
+    return (
+        <Navbar className="bg-light justify-content-between">
+            <Container>
+                <Navbar.Brand variant="primary"> Frankenstein DeFi DAO</Navbar.Brand>
+                <Navbar.Text> 
+                    {/* <Account/>  */}
+                    0xACC
+                </Navbar.Text>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
