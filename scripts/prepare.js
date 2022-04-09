9297b92add56749c5c0bd0e2f0df57827890622f
Albert Garuda <algarud@gmail.com>
2022-04-09 23:36:43 +0300

    prepare.js and styling proposals list

---
+++ scripts/prepare.js
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const [owner, voter1, voter2, voter3, voter4] = await ethers.getSigners();
+
+  // We get the contract to deploy
+  const MockFocusPool = await hre.ethers.getContractFactory("MockFocusPool");
+  const mockFocusPool = await MockFocusPool.deploy();
+  await mockFocusPool.deployed();
+  console.log("MockFocusPool deployed to:", mockFocusPool.address);
+  await (await mockFocusPool.mockLiquidity(voter1.address)).wait();
+  await (await mockFocusPool.mockLiquidity(voter2.address)).wait();
+  await (await mockFocusPool.mockLiquidity(voter3.address)).wait();
+  await (await mockFocusPool.mockLiquidity(voter4.address)).wait();
+  
+  const FrankensteinDAO = await hre.ethers.getContractFactory("FrankensteinDAO");
+  const frankensteinDAO = await FrankensteinDAO.deploy(mockFocusPool.address);
+  await frankensteinDAO.deployed();
+  console.log("FrankensteinDAO deployed to:", frankensteinDAO.address);
+  await (await mockFocusPool.setGovernance(frankensteinDAO.address)).wait();
+
+  const MockRobot = await hre.ethers.getContractFactory("MockRobot");
+  const mockRobot = await MockRobot.deploy();
+  await mockRobot.deployed();
+  console.log("MockRobot deployed to:", mockRobot.address);
+
+  console.log("signer: " + voter1.address);
+  // bad
+  // await (await frankensteinDAO.connect(voter1).propose(0, 10000, [5])).wait();
+  // await (await frankensteinDAO.connect(voter1).propose(1, 9000, [6])).wait();
+  // await (await frankensteinDAO.connect(voter1).propose(3, 9000, [7, mockRobot.address])).wait();
+
+  // good
+  await (await frankensteinDAO.connect(voter1).propose(0, 10000, [11])).wait()
+  await (await frankensteinDAO.connect(voter1).propose(1, 9100, [12])).wait();
+  await (await frankensteinDAO.connect(voter1).propose(1, 10000, [13])).wait();
+  await (await frankensteinDAO.connect(voter1).propose(1, 10000, [14])).wait();
+  await (await frankensteinDAO.connect(voter1).propose(3, 9200, [15, mockRobot.address])).wait();
+ }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- webapp/src/components/Vote.js
+++ webapp/src/components/Vote.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import { getContractAddress } from "ethers/lib/utils";
 import React, { useEffect, useState } from "react";
-import { Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import abi from "../utils/FrankensteinDAO.json";
 
@@ -64,7 +64,6 @@ function VoteComponent() {
   return (<div>
 
     <h4>Vote on proposal</h4>
-    {/* <div>Proposals: {proposalCount}</div> */}
     {/* <div>
       ... selectable listing ... (clickable)
       <div>show function, parameter</div>
@@ -73,18 +72,33 @@ function VoteComponent() {
       <div>number of current votes/pool.totalSupply</div>
     </div> */}
 
-    <div className="proposals-list">
-      {proposals.map((p, index) => {
-        return (
-          <div key={index} className="proposal-item">
-            <div>op: {p.op.toString()}</div>
-            <div>p0: {p.p0.toString()}</div>
-            <div>p1: {p.p1.toHexString()}</div>
-            <div>Yes votes: {p.yesVotes.toString()}</div>
-            <div>Deadline block : {p.deadlineBlock.toString()}</div>
-            <div>i voted: {p.iVoted.toString()} </div>
-          </div>)
-      })}
+    <div style={{maxWidth: "100%", alignContent: "center"}}>
+      <div className="proposals-list" >
+        {proposals.map((p, index) => {
+          return (
+            <div key={index} className="proposal-item">
+                <div className="proposal-line">
+                  Operation: <span className="proposal-value">{p.op.toString()}</span>
+                </div>
+                <div className="proposal-line">
+                  p0: <span className="proposal-value">{p.p0.toString()}</span>
+                </div>
+                <div className="proposal-line">
+                  {p.p1 != 0x0 ? <>Robot : <span className="proposal-address">{p.p1.toHexString()}</span></> : <>&nbsp;</>}
+                </div>
+                <div className="proposal-line">
+                  Yes votes: <span className="proposal-value">{p.yesVotes.toString()}</span>
+                </div>
+                <div className="proposal-line">
+                  Deadline block : <span className="proposal-value">{p.deadlineBlock.toString()}</span>
+                </div>
+                <div className="proposal-line">
+                  I voted: <span className="proposal-value">{p.iVoted.toString()}</span>
+                </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
 
     <div>
